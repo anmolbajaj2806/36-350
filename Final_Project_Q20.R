473d6c96eed5e5b166835b30a31de06687f40b2a
Anmol Ashish Bajaj <anmolbajaj2806@Anmols-MBP-2.wv.cc.cmu.edu>
2017-12-09 17:00:36 -0500

    Bye Bye

---
+++ Final_Project_Q20.R
@@ -0,0 +1,15 @@
+
+"Hello friends,
+
+What is your main goal in life?
+
+Are you able to travel and communicate in space?
+
+Are there other extra-terristial civilizations out there?
+
+Do you guys also kill each other over who has more power?
+
+I hope to meet in peace soon.
+
+Thank You,
+Anmol Bajaj"
